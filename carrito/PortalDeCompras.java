82eea96507613757da7200f2ca36f5a776dd9d47
Pablo Lucero <plucero87@gmail.com>
2015-09-02 18:02:38 -0300

    add basic menu items2

--- carrito/PortalDeCompras.java
+++ carrito/PortalDeCompras.java
@@ -6,13 +6,43 @@ import java.awt.event.ActionListener;
 
 public class PortalDeCompras extends JFrame implements ActionListener {
 
+    private static String LISTADO_DE_COMPRAS = "Listado de compras";
+    private static String CATEGORIA1 = "Categoria I";
+    private static String CATEGORIA2 = "Categoria II";
+    private JPanel panelCategoria1, panelCategoria2, panelListadoDeCompras;
+
+
     public PortalDeCompras() {
         super("Portal de compras");
 
+        setMenus();
+        setPaneles();
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(700, 500);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new PortalDeCompras();
+    }
+
+    private void setPaneles() {
+        panelListadoDeCompras = new JPanel();
+        panelListadoDeCompras.add(new JLabel("panel Listado De Compras"));
+
+        panelCategoria1 = new JPanel();
+        panelCategoria1.add(new JLabel("panel Categoria 1"));
+
+        panelCategoria2 = new JPanel();
+        panelCategoria2.add(new JLabel("panel Categoria 2"));
+    }
+
+    private void setMenus() {
         JMenuBar barra = new JMenuBar();
 
         JMenu carrito = new JMenu("Carrito");
-        JMenuItem listadoDeCompras = new JMenuItem("Listado de compras");
+        JMenuItem listadoDeCompras = new JMenuItem(LISTADO_DE_COMPRAS);
         JMenuItem salir = new JMenuItem("Salir");
         salir.addActionListener(this);
 
@@ -20,8 +50,8 @@ public class PortalDeCompras extends JFrame implements ActionListener {
         carrito.add(salir);
 
         JMenu catalogo = new JMenu("Catalogo");
-        JMenuItem categoria1 = new JMenuItem("Categoria I");
-        JMenuItem categoria2 = new JMenuItem("Categoria II");
+        JMenuItem categoria1 = new JMenuItem(CATEGORIA1);
+        JMenuItem categoria2 = new JMenuItem(CATEGORIA2);
 
         catalogo.add(categoria1);
         catalogo.add(categoria2);
@@ -37,14 +67,6 @@ public class PortalDeCompras extends JFrame implements ActionListener {
         barra.add(ayuda);
 
         setJMenuBar(barra);
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(200, 200);
-        setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        new PortalDeCompras();
     }
 
     @Override
